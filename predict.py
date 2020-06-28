8ff07cd811ab38576db6cc1aef03f2a360c4e71c
Abid Ebna Saif Utsha <abidebnasaifutsha@gmail.com>
2020-06-28 16:37:07 +0800

    first commit

---
+++ predict.py
@@ -0,0 +1,59 @@
+"""
+Author: @ Abid Ebna Saif Utsha
+Created Date: 28/06/2020
+A simple linear Regression model to predict a country
+population.
+"""
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import re
+import json
+import warnings
+warnings.filterwarnings("ignore")
+
+
+def country_list_gen(df):
+    df.rename(columns={'Country Name':'country_name'},inplace=True)
+    df['country_name'] = df['country_name'].apply(lambda row: row.lower())
+    lists = df['country_name'].unique().tolist()
+    with open('country_list.json','w', encoding='utf-8') as f:
+        json.dump(lists, f, ensure_ascii=False,indent=4)
+    return lists, df
+
+def selecting_country(df,country):
+    """
+    this function will 
+    """
+    df = df.loc[df['country_name']==country]
+    df.drop(['country_name','Country Code','Indicator Name','Indicator Code'],axis=1,inplace=True)
+    df = df.T
+    df.dropna(inplace=True)
+    df = df.reset_index()
+    return df
+
+def prediction_model(df):
+    x = df.iloc[:, 0].values.reshape(-1,1)
+    y = df.iloc[:, 1].values.reshape(-1,1)
+    model = LinearRegression().fit(x,y)
+    return model
+
+def prediction(model, year):
+    return int(model.coef_[0][0] * year + model.intercept_[0])
+
+
+def main():
+    country = input("Please input the country name: ").lower()
+    year = int(input("Please input the year to predict: "))
+    df = pd.read_csv('pop.csv')
+    lists, df = country_list_gen(df)
+    if country in lists:
+        df = selecting_country(df, country)
+        model = prediction_model(df)
+        result = prediction(model,year)
+        print(f"{country.upper()} population in {year} will be {result}")
+    else:
+        print('kindly check country name spelling from country_list.json')
+    
+if __name__ == "__main__":
+    main()
